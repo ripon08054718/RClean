4c8c66310f69b304ec0ba6bccf37c0d5c0931ce9
Md. Sahidul Islam <ripon.ru.statistics@gmail.com>
2015-05-13 20:19:57 +0600

    Update checkRange.r

--- checkRange.r
+++ checkRange.r
@@ -1,5 +1,6 @@
-checkRange<-function(x,low_limit,up_limit)
+checkRange<-function(x,low_limit,up_limit,other_value)
 {
+   if(missing(other_value)){
 	for (i in 1:length(x))
 	{
 	if((x[i]<low_limit)||(x[i]>up_limit))
@@ -11,5 +12,20 @@ checkRange<-function(x,low_limit,up_limit)
 		}
 	
 	}
+	}else{
+	for (i in 1:length(x))
+	{
+	if((x[i]<low_limit)||(x[i]>up_limit)){
+	  for(j in 1:length(other_value))
+		{
+		if (x[i]!=other_value[j])
+		{
+		RecNo<-i
+		SlNo<-i
+		Field_Value<-x[i]
+		out<-cat(sprintf(" %d   %d    %d         Filed value is out of range\n", RecNo, SlNo, Field_Value))
+		}}
+		}
+	}	
+	}	
 }
-
